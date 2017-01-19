08accdfeca8db0767c7ffffb88a1072aa12389a9
Santiago Ferreira <san650@gmail.com>
2017-01-18 21:36:26 -0300

    Move dashboard to in-repo-addon

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 /* jshint node: true */
 'use strict';
 
-var EngineAddon = require('ember-engines/lib/engine-addon');
-
-module.exports = EngineAddon.extend({
+module.exports = {
   name: 'telling-stories-dashboard',
-  lazyLoading: false,
-
-  included: function(app) {
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
-
-    this.app = app;
-  },
 
   isDevelopingAddon() {
     return true;
   }
-});
+};
