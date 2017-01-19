5144e63a756c4c0b0f22676040dc885fc8070fea
Santiago Ferreira <san650@gmail.com>
2017-01-18 21:42:28 -0300

    Update ember-cli@2.10.1

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,6 @@
 /*jshint node:true*/
 'use strict';
 
-module.exports = function(environment/*, appConfig */) {
-  var ENV = {
-    modulePrefix: 'telling-stories-dashboard',
-    environment: environment
-  }
-
-  return ENV;
+module.exports = function(/* environment, appConfig */) {
+  return { };
 };
